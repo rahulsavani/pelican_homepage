0d815a2c2c2891d70e02ed0cffa553071adce0b9
Rahul Savani <rahul.savani@gmail.com>
2016-02-17 22:08:43 +0000

    SITEURL in publishconf.py changed to rahulsavani.uk

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,8 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://cgi.csc.liv.ac.uk/~rahul'
+#SITEURL = 'http://cgi.csc.liv.ac.uk/~rahul'
+SITEURL = 'http://rahulsavani.uk'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = None
